79ba31c258c0ffe3f18ad75aa6f88cf5642fe34b
kmlee95 <kmlee95@naver.com>
2019-06-17 17:44:41 +0900

    cli

--- secondprojcet/settings.py
+++ secondprojcet/settings.py
@@ -19,10 +19,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'cv!)9f$bqc2lt%%=p26ls7m9xlh^3!f^0767c5cc52_4+^vmj#'
+SECRET_KEY = os.environ.get(
+    'DJANGO_SECRET_KEY', 'cv!)9f$bqc2lt%%=p26ls7m9xlh^3!f^0767c5cc52_4+^vmj#')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = []
 
