3fac1f88b34fd4d7c83e0d59af7d76231f6a8748
kmlee95 <kmlee95@naver.com>
2019-06-30 03:23:53 +0900

    search

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,9 @@ urlpatterns = [
     path('<int:blog_id>',views.detail,name='detail'),
     path('edit/<int:blog_id>',views.edit, name="edit"),
     path('delete/<int:blog_id>',views.delete, name="delete"),
-    path('create',views.create, name='create'),
     path('newblog/', views.blogpost, name="newblog"),
+    path('search', views.search, name="search"),
 ]
+
+
+#name은 html에서 이름으로 사용하기위해 작성.
